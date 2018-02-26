ae7bada4e06f50d376e9dfffe4aec38246eaa19a
lahbib BELHADDAD <belhaddadl80@gmail.com>
2018-02-26 17:39:24 +0100

    review model

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+
+  get 'reviews/create'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :tickets do
+    resources :reviews, only: [:index, :new, :create ]
+  end
+  resources :reviews, only: [ :show, :edit, :update, :destroy ]
 end
