1ea1e681b4ab993052659b7d91d9402f211d0b8a
isabelle pontoizeau <isa.pontoizeau@gmail.com>
2018-02-26 17:24:38 +0100

    add belongs_to and model ticket

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :alumni_tickets, class_name: 'Ticket', foreign_key: :alumni_id
+  has_many :mentor_tickets, class_name: 'Ticket', foreign_key: :mentor_id
 end
