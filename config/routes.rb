4d98323a9be3cd3754f7d71cbbeacfe217a9abc3
lahbib BELHADDAD <belhaddadl80@gmail.com>
2018-02-27 09:07:24 +0100

    aujourd'hui

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,18 @@
 Rails.application.routes.draw do
+  get 'mentor_profils/new'
+
+  get 'mentor_profils/create'
+
+  get 'mentor_profils/update'
+
+  get 'mentor_profils/destroy'
+
+  get 'mentor_profils/show'
+
+  get 'mentor_profils/edit'
+
+  get 'mentor_profils/index'
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
