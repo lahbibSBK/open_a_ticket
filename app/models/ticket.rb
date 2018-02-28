47bebe1f1da3557cd4aa48606740cfa12d8fcbb4
mathieu nicolas <matni9475@gmail.com>
2018-02-28 16:56:53 +0100

    form

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,11 @@
 class Ticket < ApplicationRecord
+  PRIORITIES = ["high", "medium", "low"]
+  SKILLS = ["HTML", "CSS", "RUBY ON RAILS", "RUBY", "PHP", "IOS", "ANGULAR JS", "PYTHON", "ANDROID", "MYSQL", "WORDPRESS", "XCODE", "JAVA", "C", "NODE JS", "ASP.NET", "BOOTSTRAP", "SHOPIFY", "GITHUB", "GO", "HEROKU", "REACT", "MONGO DB", "MAGENTO", "POSTGRESQL", "SEO", "UX/UI" ]
+
+
   belongs_to :mentor, class_name: 'User', foreign_key: :mentor_id
   belongs_to :alumni, class_name: 'User', foreign_key: :alumni_id
   has_one :review
 
   validates :title, :alumni_id, :mentor_id, :ticket_skills, :ticket_location, :ticket_duration, :price,  presence: true
-
 end
