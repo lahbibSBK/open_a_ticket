1d615e7527e1b6a1e4ced8b134d5d1130a32b11a
lahbib BELHADDAD <belhaddadl80@gmail.com>
2018-02-28 15:13:26 +0100

    link between reviews and mentor

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,8 +1,8 @@
 class Ticket < ApplicationRecord
-  belongs_to :mentor, class_name: 'User', foreign_key: :mentor_id
+  belongs_to :mentor_profil
   belongs_to :alumni, class_name: 'User', foreign_key: :alumni_id
   has_one :review
 
-  validates :title, :alumni_id, :mentor_id, :ticket_skills, :ticket_location, :ticket_duration, :price,  presence: true
+  validates :title, :alumni_id, :mentor_profil_id, :ticket_skills, :ticket_location, :ticket_duration, :price,  presence: true
 
 end
