b89b937a7533aa5c4afd501b6d372798397885d8
kevcha <kevin.chavanne@gmail.com>
2018-03-05 14:01:19 +0100

    Remove mentor_skills in favor of tag_names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180305125008) do
+ActiveRecord::Schema.define(version: 20180305130039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -41,7 +41,6 @@ ActiveRecord::Schema.define(version: 20180305125008) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "mentor_skills"
     t.index ["user_id"], name: "index_mentor_profils_on_user_id"
   end
 
