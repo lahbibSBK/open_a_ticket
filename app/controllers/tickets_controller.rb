d8e05a41a1c6d5115bb5fbbd6a0c07d5fbee7abc
Lahbib Belhaddad <belhaddadl80@gmail.com>
2018-03-05 20:57:10 +0100

    modify match mentor taking in consideration price

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -98,9 +98,17 @@ class TicketsController < ApplicationController
   end
 
   def match_mentors
-    @match_mentors_list = MentorProfil.tagged_with(
+    match_mentors = []
+    @match_mentors_list = []
+    match_mentors = MentorProfil.tagged_with(
       names: @ticket.tag_names,
       match: :any
     )
+
+    match_mentors.each do |mentor|
+      if mentor.minimum_price.to_i <= @ticket.price.to_i
+        @match_mentors_list << mentor
+      end
+    end
   end
 end
