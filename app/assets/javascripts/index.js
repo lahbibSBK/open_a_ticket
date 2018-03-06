246ad8fe067551da0fc9202ef2aa3b2957731c94
Lahbib Belhaddad <belhaddadl80@gmail.com>
2018-03-06 15:02:03 +0100

    running after merge

--- app/assets/javascripts/index.js
+++
@@ -1,6 +0,0 @@
-const onglet = document.getElementById("onglet");
-onglet.addEventListener("click", (event) => {
-  console.log("hello from JS");
-  console.log(event.currentTarget);
-  event.currentTarget.classList.toggle("red-onglet");
-});
