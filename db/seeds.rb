2ebdc23bcde6202ba9469f1790a3eec2cf76660a
Lahbib Belhaddad <belhaddadl80@gmail.com>
2018-03-08 19:12:19 +0100

    modif seed lahbib mentor and remove first name in review

--- db/seeds.rb
+++ db/seeds.rb
@@ -69,7 +69,7 @@ Chez nous on utilise Slack, Asana et d'autre, des outils modernes, et du coup au
 Si demain j'ai une idée (et qu'elle est bonne ^^) elle peut être mise en place le surlendemain. (= Salarié heureux :-) )",
   minimum_price: "25",
   user_id: jos.id,
-  tag_names: ['HTML', 'CSS', 'RUBY ON RAILS', 'RUBY', 'PHP', 'IOS', 'ANGULAR JS', 'PYTHON', 'ANDROID', 'MYSQL', 'WORDPRESS', 'XCODE', 'JAVA', 'C', 'NODE JS', 'ASP.NET', 'BOOTSTRAP', 'SHOPIFY', 'GITHUB', 'GO', 'HEROKU', 'REACT', 'MONGO DB', 'MAGENTO', 'POSTGRESQL', 'SEO', 'UX/UI']
+  tag_names: ['HTML', 'CSS', 'RUBY ON RAILS', 'RUBY', 'NODE JS', 'BOOTSTRAP', 'GITHUB']
 )
 
 kev = User.create!(
@@ -91,7 +91,7 @@ kevinmentor = MentorProfil.create!(
   Co-GérantCraftsmen 2010 - 2016
   Durant 6 ans, nous avons créé, mon ex associé et moi même une société de développement d’applications webs et mobiles.Nos clients étaient : - Des personnes souhaitant développer un produit web, et nous les accompagnions à la fois sur le côté business, pilotage du produit, mais également sur le développement du premier prototype (MVP), pour leur permettre de valider leur marché.- Des équipes techniques ayant besoin de formations de mise à niveau pour une technologie précise ou d’un accompagnement provisoire sur le développement de leur produit.Ces quelques années nous ont permis d’apprendre plusieurs technologies, en nous apportant le recul nécessaire quant aux choix d’architecture, technologiques, et business à faire.",
   minimum_price: "30",
-  tag_names: ['HTML', 'CSS', 'RUBY ON RAILS', 'RUBY', 'PHP', 'IOS', 'ANGULAR JS', 'PYTHON', 'ANDROID', 'MYSQL', 'WORDPRESS', 'XCODE', 'JAVA', 'C', 'NODE JS', 'ASP.NET', 'BOOTSTRAP', 'SHOPIFY', 'GITHUB', 'GO', 'HEROKU', 'REACT', 'MONGO DB', 'MAGENTO', 'POSTGRESQL', 'SEO', 'UX/UI'],
+  tag_names: ['HTML', 'CSS', 'RUBY ON RAILS', 'RUBY', 'ANGULAR JS', 'MYSQL', 'BOOTSTRAP', 'GITHUB', 'HEROKU'],
   user_id: kev.id
 )
 
@@ -147,7 +147,6 @@ mat = User.create!(
 
 puts 'Creating ...'
 toto = ["fr", "gb"]
-SKILL = ['HTML', 'CSS', 'RUBY ON RAILS', 'RUBY', 'PHP', 'IOS', 'ANGULAR JS', 'PYTHON', 'ANDROID', 'MYSQL', 'WORDPRESS', 'XCODE', 'JAVA', 'C', 'NODE JS', 'ASP.NET', 'BOOTSTRAP', 'SHOPIFY', 'GITHUB', 'GO', 'HEROKU', 'REACT', 'MONGO DB', 'MAGENTO', 'POSTGRESQL', 'SEO', 'UX/UI']
 # puts 'Creating X-Seeds (Users as Mentor/ Ticket Open/pending/closed / Review...'
 # User.all.each do |user|
 #   if user.last_name.downcase.exclude?("pontoizeau")
@@ -236,7 +235,7 @@ davidhass = User.create!(
   address: "the Foundation for Law and Government (FLAG) XXXXX In the Truck",
   batch_wagon: "001",
   pic_url: "http://idata.over-blog.com/1/71/60/02//228011-david_hasselhoff_RED.jpg",
-  speaking_language: ["fr", "gb", "ma", "Le Voiture", "Le Mec bourré"],
+  speaking_language: ["fr", "gb", "ma"],
   slack_id: "U8NTWTTJL",
 )
 davidmentor = MentorProfil.create!(
@@ -307,7 +306,7 @@ end
 m1 = MentorProfil.where(user: User.where(first_name: "Clément")).first
 m1.experience = "Scientific software developer (image processing, data analysis, machine learning & visualisation) and computing using C++, matlab, javascript & web apps.
 
-I've been developing working professionally for over 15 years and have touched all sorts of technologies, APIs, Algorithms, build systems in that time 3D Visualisation, GPU/CUDA, Linux/Windows, boost, node.js, ....
+I've been developing working professionally for over 5 years and have touched all sorts of technologies, APIs, Algorithms, build systems in that time 3D Visualisation, GPU/CUDA, Linux/Windows, boost, node.js, ....
 
 These days getting into js, node and web technologies. I practice BDD & TDD have led & worked on agile teams and regularly teach people over the internet."
 m1.save
@@ -347,13 +346,6 @@ m1 = MentorProfil.where(user: User.where(last_name:"Perrin")).first
 m1.experience = "I am 2016 graduate batch from LeWagon Lyon having Bachelors degree in Mathematics. From then I have worked with a breadth of companies from software giant EBAY to a 1 year old startups. In EBAY, I have lead a team of 4 engineers. I have solid grasp of software engineering and worked with very interesting technologies."
 m1.save
 
-MentorProfil.create!(
-  experience: "I am a freelance web developer working out of Morrocco. My weapons of choices are : HTML, CSS, Javascript, SQL, Ruby, Ruby on Rails, React JS. I literally spend as much hours as possible every day on coding.",
-  minimum_price: 20,
-  user_id: User.where(email:"belhaddadl80@gmail.com").first.id,
-  tag_names: ['HTML', 'CSS', 'RUBY ON RAILS', 'RUBY', 'PHP', 'IOS', 'ANGULAR JS', 'PYTHON', 'ANDROID', 'MYSQL', 'WORDPRESS', 'XCODE', 'JAVA', 'C', 'NODE JS', 'ASP.NET', 'BOOTSTRAP', 'SHOPIFY', 'GITHUB', 'GO', 'HEROKU', 'REACT', 'MONGO DB', 'MAGENTO', 'POSTGRESQL', 'SEO', 'UX/UI']
-)
-
 
 # pour le geocoder dans match_mentor
 # const = []
@@ -391,15 +383,15 @@ reviewstock =[["Knows his stuff, very quick and professional.",3],
 ["Patient and knowlagable",4],
 ["Super helpful, great javascript skills.",2],
 ["Professional, punctual, and courteous. Friendly guy with talent.",4],
-["Michael helped me work through Git installation and update issues on Mac OS",2],
-["Michael is knowledgeable and knows about Angular, CSS, and Heroku deployment. Very nice and friendly person. He is also someone that has been coding for a while.",2],
-["Micheal was a big help. He's very knowledgeable and a good communicator.",4],
-["Really great session with Michael. A lot of good communication before session. Starting out, we explored what I was trying to do, and when we hit a little wall, Michael was willing to suspend the session until we got back on track. Then he methodically led me to the right solution. Really like the approach, and will make it my 'leading practice' for testing.",2],
+["He helped me work through Git installation and update issues on Mac OS",2],
+["He is knowledgeable and knows about Angular, CSS, and Heroku deployment. Very nice and friendly person. He is also someone that has been coding for a while.",2],
+["He was a big help. He's very knowledgeable and a good communicator.",4],
+["Really great session with him. A lot of good communication before session. Starting out, we explored what I was trying to do, and when we hit a little wall, He was willing to suspend the session until we got back on track. Then he methodically led me to the right solution. Really like the approach, and will make it my 'leading practice' for testing.",2],
 ["This guy is awesome. He was extremely helpful, provided example code for reference and helpful links for further reading on topics. He is definitely an expert! I highly recommend.",2],
-["It was a pleasure working with Mike. He is very experienced with Node, javascript, and javascript promises. He is also very patient and is an amazing and outstanding mentor. I look forward to working with Mike more over the next few months.",4],
+["It was a pleasure working with Him. He is very experienced with Node, javascript, and javascript promises. He is also very patient and is an amazing and outstanding mentor. I look forward to working with Mike more over the next few months.",4],
 ["Excellent help! Solved by problem and gave good advice on Angular handling.",2],
 ["Amazing mentor!",3],
-["The best CodeMentor of all time.",4],
+["The best Mentor of all time.",4],
 ["he was a great in helping me solve a business issue I was stuck on with my Rails application. He quickly understood my needs and was a pleasure to work with. Would highly recommend.",5],
 ["Fast and effective. : )",2],
 ["he is terrific! Thank you so much for your help! Really knowledgeable, really quick and really professional! Many kudos!",3],
@@ -414,27 +406,73 @@ reviewstock =[["Knows his stuff, very quick and professional.",3],
 mentorall = MentorProfil.all
 100.times do
   ticket = []
-      toto = ["fr", "gb"]
-      rand(4).times do
-        toto << Faker::Address.country_code
-      end
-      ticket = Ticket.create!(
-        title: Faker::ChuckNorris.fact,
-        alumni_id: users89.sample.id,
-        mentor: mentorall.sample.user,
-        content: Faker::HowIMetYourMother.quote,
-        tag_names: SKILLS.sample(3),
-        ticket_location: Faker::Address.city,
-        ticket_duration: "#{rand(52)} hours",
-        price_cents: Faker::Number.number(2),
-        priority: ["low", "medium", "high"].sample,
-        status: "closed",
-        speaking_language: toto,
-      )
+  toto = ["fr", "gb"]
+  rand(4).times do
+    toto << Faker::Address.country_code
+  end
+  ticket = Ticket.create!(
+    title: Faker::ChuckNorris.fact,
+    alumni_id: users89.sample.id,
+    mentor: mentorall.sample.user,
+    content: Faker::HowIMetYourMother.quote,
+    tag_names: SKILLS.sample(3),
+    ticket_location: Faker::Address.city,
+    ticket_duration: "#{rand(52)} hours",
+    price_cents: Faker::Number.number(2),
+    priority: ["low", "medium", "high"].sample,
+    status: "closed",
+    speaking_language: toto,
+  )
 
-      Review.create!(
-        ticket_id: ticket.id,
-        description: reviewstock.sample[0],
-        rating: rand(1..5).to_i
-      )
+  Review.create!(
+    ticket_id: ticket.id,
+    description: reviewstock.sample[0],
+    rating: rand(2..5).to_i
+  )
+end
+
+#Lahbib Mentor Seed
+lahmentor = MentorProfil.create!(
+  experience: "I am a freelance web developer working out of Morrocco. My weapons of choices are : HTML, CSS, Javascript, SQL, Ruby, Ruby on Rails, React JS. I literally spend as much hours as possible every day on coding.",
+  minimum_price: 20,
+  user_id: User.where(email:"belhaddadl80@gmail.com").first.id,
+  tag_names: ['HTML', 'RUBY ON RAILS', 'RUBY', 'MYSQL', 'BOOTSTRAP', 'GITHUB', 'HEROKU']
+)
+reviewlahbib =[["Knows his stuff, very quick and professional.",3],
+["Another great session with him. Useful and insightful I appreciate his help!",3],
+["Always a good experience with him. He is as always very understanding and helpful.",4],
+["As always, a great learning experience !",5],
+["he is simply awesome and fixed problems where three others couldn't. Really highly recommended",3],
+["Excellent. Amazing. He knows what he is talking about. And is truthful when he does not know something.",5],
+["kidnly helped us to resolve some issues with our protractor, which was not running due to missing install files",2],
+["It is the best mentor I've worked with, surely a great person to learn from.",5],
+["Wow. Masterful. I could not recommend more.",3],
+["Patient and knowlagable",4]
+]
+
+reviewlahbib.each do |review|
+  ticket = []
+  toto = ["fr", "gb", "es"]
+  rand(4).times do
+    toto << Faker::Address.country_code
+  end
+  ticket = Ticket.create!(
+    title: Faker::ChuckNorris.fact,
+    alumni_id: users89.sample.id,
+    mentor: lahmentor.user,
+    content: Faker::HowIMetYourMother.quote,
+    tag_names: SKILLS.sample(3),
+    ticket_location: Faker::Address.city,
+    ticket_duration: "#{rand(52)} hours",
+    price_cents: Faker::Number.number(2),
+    priority: ["low", "medium", "high"].sample,
+    status: "closed",
+    speaking_language: toto,
+  )
+
+  Review.create!(
+    ticket_id: ticket.id,
+    description: review[0],
+    rating: review[1].to_i
+  )
 end
